1bdefa3d961733fa9efe86aa13db0cba23643179
argetlam <marrislucas@gmail.com>
2025-12-02 16:57:27 -0500

    Line number and curly brace matching template

--- src/ui/Editor.cpp
+++ src/ui/Editor.cpp
@@ -1,5 +1,7 @@
 #include "Editor.hpp"
 
+#include <string>
+
 Editor::Editor(unsigned int bufferSize, unsigned int width, unsigned int height) {
     this->inputTextBuffer = new char[bufferSize];
     this->inputTextBuffer[0] = '\0';
@@ -13,17 +15,56 @@ Editor::Editor(unsigned int bufferSize, unsigned int width, unsigned int height)
     this->inspectorHeight = 100;
 }
 
+static int EditorInputCallback(ImGuiInputTextCallbackData* data) {
+    if (data->EventFlag == ImGuiInputTextFlags_CallbackEdit) {
+        bool nextLineTrigger = data->BufTextLen > 0 && data->Buf[data->CursorPos - 1] == '\n';
+        bool previousOpenBrace = data->BufTextLen > 1 && data->Buf[data->CursorPos - 2] == '{';
+        if (nextLineTrigger && previousOpenBrace) {
+            const char* closeBrace = "\t\n}";
+            data->InsertChars(data->CursorPos, closeBrace);
+
+            data->CursorPos -= 2;
+        }
+    }
+
+    return 0;
+}
+
 void Editor::render() {
     ImGui::SetNextWindowSize(ImVec2((float) this->width, (float) this->height), ImGuiCond_Once);
+
     ImGui::Begin("Editor");
-   
+
+    ImGuiTableFlags lineNumberFlags = ImGuiTableFlags_BordersInnerV;
+    ImGuiInputTextFlags textBoxFlags = ImGuiInputTextFlags_AllowTabInput | ImGuiInputTextFlags_CallbackEdit;
+
+    ImGui::BeginTable("##LineNumContainer", 2, lineNumberFlags, ImVec2(-1, -1));
+    ImGui::TableSetupColumn("##LineNumberColumn", ImGuiTableColumnFlags_WidthFixed, 10);
+    ImGui::TableSetupColumn("##EditorColumn", ImGuiTableColumnFlags_WidthStretch);
+
+    ImGui::TableNextRow();
+    ImGui::TableSetColumnIndex(0);
+
+    ImGui::BeginChild("##LineNumberChild", ImVec2(-1, -1), false, ImGuiWindowFlags_NoScrollbar);
+
+    int totalLines = 9;
+    for (int i = 1; i <= totalLines; i++) {
+        ImGui::TextUnformatted(std::to_string(i).c_str());
+    }
+
+    ImGui::EndChild();
+
+    ImGui::TableSetColumnIndex(1);
     ImGui::InputTextMultiline(
         "##EditorTextBox",
         this->inputTextBuffer,
         this->bufferSize,
         ImVec2(-1, -1),
-        ImGuiInputTextFlags_AllowTabInput
+        textBoxFlags,
+        EditorInputCallback
     );
+
+    ImGui::EndTable();
 
     ImGui::End();
 
@@ -35,6 +76,8 @@ void Editor::render() {
     ImGui::End();
 }
 
+
+
 void Editor::destroy() {
-    delete[] this->inputTextBuffer;
+    free(this->inputTextBuffer);
 }
