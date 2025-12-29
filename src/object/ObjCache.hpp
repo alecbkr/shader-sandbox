38eab9cdfa1e3f88c3839a5ce0f59bb06c4e9a4f
Micah Ramirez <supermicah1@gmail.com>
2025-12-29 16:22:32 -0500

    make objCache public. this is a temp fix, cause I need a way to access all of the objects, or at least their ids.

--- src/object/ObjCache.hpp
+++ src/object/ObjCache.hpp
@@ -23,10 +23,11 @@ class ObjCache {
         static void renderAll(glm::mat4 projection, glm::mat4 view);
         static void printOrder();
 
+        static std::vector<std::unique_ptr<Object>> objCache;
+
     private:
         static Object* getObject(const std::string name);
         static void reorderByProgram();
-        static std::vector<std::unique_ptr<Object>> objCache;
         static std::unordered_map<std::string, Object*> objMap;
         
 };
