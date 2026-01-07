8098e52526e24e4559a4ae33e91eb5530b13cef6
DG-1337 <devgopal745@gmail.com>
2026-01-07 10:31:48 -0500

    commented out deprecated code

--- src/engine/Logger.cpp
+++ src/engine/Logger.cpp
@@ -1,40 +1,40 @@
-#include "Logger.hpp"
+// #include "Logger.hpp"
 
-namespace shbx {
-    std::vector<LogEntry> Logger::logs; 
-    LogLevel Logger::abortWhen = LogLevel::CRITICAL; // Might need to adjust this depending on how we want the program to crash
+// namespace shbx {
+//     std::vector<LogEntry> Logger::logs; 
+//     LogLevel Logger::abortWhen = LogLevel::CRITICAL; // Might need to adjust this depending on how we want the program to crash
 
-    void Logger::addLog(shbx::LogLevel level, const char *src, const char *msg, int lineNum) {
-        LogEntry newLog = {.level = level, .src = src, .msg = msg, .lineNum = lineNum};
-        logs.push_back(newLog);
+//     void Logger::addLog(shbx::LogLevel level, const char *src, const char *msg, int lineNum) {
+//         LogEntry newLog = {.level = level, .src = src, .msg = msg, .lineNum = lineNum};
+//         logs.push_back(newLog);
         
-        // TODO: if we need to store the logs in a file 
+//         // TODO: if we need to store the logs in a file 
 
-        // TODO: if we want to output to stdout aswell 
+//         // TODO: if we want to output to stdout aswell 
 
-        // TODO: handle abort logic (throw exception or crash)
-    }
+//         // TODO: handle abort logic (throw exception or crash)
+//     }
 
-    void Logger::addLog(shbx::LogLevel level, const char *src, const std::string msg, int lineNum) {
-        LogEntry newLog = {.level = level, .src = src, .msg = msg, .lineNum = lineNum}; 
-        logs.push_back(newLog); 
+//     void Logger::addLog(shbx::LogLevel level, const char *src, const std::string msg, int lineNum) {
+//         LogEntry newLog = {.level = level, .src = src, .msg = msg, .lineNum = lineNum}; 
+//         logs.push_back(newLog); 
 
-        // TODO: if we need to store the logs in a file 
+//         // TODO: if we need to store the logs in a file 
 
-        // TODO: if we want to output to stdout aswell 
+//         // TODO: if we want to output to stdout aswell 
 
-        // TODO: handle abort logic (throw exception or crash)
-    }
+//         // TODO: handle abort logic (throw exception or crash)
+//     }
 
-    void Logger::clearLogs() {
-        logs.clear(); 
-    }
+//     void Logger::clearLogs() {
+//         logs.clear(); 
+//     }
 
-    void Logger::setAbortWhen(LogLevel level) {
-        Logger::abortWhen = level; 
-    }
+//     void Logger::setAbortWhen(LogLevel level) {
+//         Logger::abortWhen = level; 
+//     }
 
-    const std::vector<LogEntry>& Logger::getLogs() {
-        return Logger::logs; 
-    }
-}; 
+//     const std::vector<LogEntry>& Logger::getLogs() {
+//         return Logger::logs; 
+//     }
+// }; 
