b5759842f734555d9fb0ec8ffdf33ac5d8d84528
Dev Gopal <devgopal745@gmail.com>
2026-01-08 13:48:17 -0500

    reformated output logs

--- src/core/logging/StdoutSink.cpp
+++ src/core/logging/StdoutSink.cpp
@@ -1,18 +1,17 @@
 #include "StdoutSink.hpp"
 
 void StdoutSink::addLog(const LogEntry& entry) {
-    int idx = std::min((int)entry.level, 4); // clamp the index to avoid out-of-bounds
     std::stringstream alert; 
 
     switch (entry.level) {
-        case    LogLevel::CRITICAL:  alert << BG_RED << "CRITICAL:" << RESET_COLORS << " ";          break; 
-        case    LogLevel::ERROR:     alert << TEXT_RED << "ERROR:" << RESET_COLORS << " ";           break; 
-        case    LogLevel::WARNING:   alert << TEXT_WARNING << "WARNING:" << RESET_COLORS << "";      break; 
-        case    LogLevel::INFO:      alert << "INFO: ";                                              break;
-        default:                     alert << "ANOMALY: ";                                           break; 
+        case    LogLevel::CRITICAL:  alert << BG_RED << "CRITICAL: " << RESET_COLORS << " ";          break; 
+        case    LogLevel::ERROR:     alert << TEXT_RED << "ERROR: " << RESET_COLORS << " ";           break; 
+        case    LogLevel::WARNING:   alert << TEXT_WARNING << "WARNING: " << RESET_COLORS << "";      break; 
+        case    LogLevel::INFO:      alert << "INFO: ";                                               break;
+        default:                     alert << "ANOMALY: ";                                            break; 
     }    
 
-    std::cout << alert.str() << entry.msg << std::endl; 
+    std::cout << '[' << alert.str() << entry.src << "] " << entry.msg << std::endl; 
 }
 
 
