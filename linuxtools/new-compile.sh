b9ea950dc8f923b3a2ea604113df29f3ef3e50d6
Micah Ramirez <supermicah1@gmail.com>
2026-01-16 00:57:41 -0500

    remove linuxtools as they were specific to my setup (micah)

--- linuxtools/new-compile.sh
+++
@@ -1,7 +0,0 @@
-cd ..
-cmake -S . -B build -DCMAKE_TOOLCHAIN_FILE=./clang-toolchain.cmake
-
-cd build
-cmake --build .
-cd ../.mytools
-
--- linuxtools/run.sh
+++
@@ -1,4 +0,0 @@
-cd ../build
-cmake --build .
-cd ../.mytools
-prime-run gamescope -W 1920 -H 1080 -r 60 --force-grab-cursor -- ../bin/sandbox
