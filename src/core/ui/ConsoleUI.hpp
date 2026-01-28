fb416e92342e8b1a399357c9cf905a8e7cd4e410
Dev Gopal <devgopal745@gmail.com>
2026-01-28 18:22:41 -0500

    Made info colors consistent with stdout and the console

--- src/core/ui/ConsoleUI.hpp
+++ src/core/ui/ConsoleUI.hpp
@@ -28,7 +28,7 @@ static const ImVec4 LOG_COLORS[] = {
     ImVec4(1.0f, 0.0f, 0.0f, 1.0f),         // Critical (Deep Red) 
     ImVec4(1.0f, 0.4f, 0.4f, 1.0f),         // Error (lighter red)
     ImVec4(1.0f, 0.1f, 0.5f, 1.0f),         // Warning (Magenta)
-    ImVec4(1.0f, 1.0f, 1.0f, 1.0f),         // Info (White)
+    ImVec4(0.4f, 1.0f, 0.4f, 1.0f)          // Info (Light Green)
                                             // Anomaly (light gray)
 }; 
 
