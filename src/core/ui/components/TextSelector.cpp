3c0bce66aa46e555fc612ef10e70314c790324d5
Dev Gopal <devgopal745@gmail.com>
2026-02-07 18:53:40 -0500

    Implemented the necessary function to call on the widget

--- src/core/ui/components/TextSelector.cpp
+++ src/core/ui/components/TextSelector.cpp
@@ -1,68 +1,178 @@
 #include "TextSelector.hpp"
 #include <cctype>
 #include <limits>
+#include <iostream>
 
-void TextSelector::drawRowSelection(ImDrawList* drawList, const TextSelectionCtx& ctx, const TextSelectionMetrics& metrics, int currRow) {
-    if (!ctx.active) return; 
+static struct TextSelectorState {
+    TextSelectionCtx* userCtx = nullptr; 
 
-    int selMin, selMax; 
-    ctx.getNormalizedRows(selMin, selMax); 
-    if (currRow < selMin || currRow > selMax) return; 
+    ImVec2 origin; 
+    float lineHeight; 
+    float charWidth; 
+    float maxWidth; 
+    ImU32 highlightColor; 
 
-    float hlStart = 0.0f; 
-    float hlEnd = metrics.maxWidth; 
+    bool isPendingWordSelect = false; 
+    int hoveredRow = -1; 
+    int hoveredCol = -1; 
+} txtState;
 
-    // selecting a single line
-    if (selMin == selMax) {
-        int sCol = std::min(ctx.startCol, ctx.endCol); 
-        int eCol = std::max(ctx.startCol, ctx.endCol); 
-        hlStart = sCol * metrics.charWidth;
-        hlEnd = (eCol + 1) * metrics.charWidth;    
-    }
+bool TextSelector::Begin(const char* id, int totalRows, TextSelectionCtx& ctx, ImU32 highlightCol) {
+    if (!isFontMonospace()) return false; 
 
-    // top line of multi-selection
-    if (currRow == selMin) {
-        int sCol = (ctx.startRow < ctx.endRow) ? ctx.startCol : ctx.endCol; 
-        hlStart = sCol * metrics.charWidth; 
-        hlEnd = metrics.maxWidth; 
+    txtState.userCtx = &ctx; 
+    txtState.lineHeight = std::max(1.0f, ImGui::GetTextLineHeight()); 
+    txtState.charWidth = ImGui::CalcTextSize("A").x; 
+    txtState.maxWidth = ImGui::GetContentRegionAvail().x; 
+    txtState.isPendingWordSelect = false; 
+
+    // draw the invisible "hitbox" that we use to select from 
+    float totalHeight = (float)totalRows *txtState.lineHeight; 
+    ImGui::InvisibleButton(id, ImVec2(txtState.maxWidth, totalHeight)); 
+
+    // capture the screen pos of the content area 
+    txtState.origin = ImGui::GetItemRectMin(); 
+
+    // handle mouse clicks 
+    if (ImGui::IsItemHovered() || ImGui::IsItemActive()) {
+        ImVec2 mouse = ImGui::GetMousePos(); 
+        float relY = mouse.y - txtState.origin.y;
+        float relX = mouse.x - txtState.origin.x; 
+
+        txtState.hoveredRow = (int)std::floor(relY / txtState.lineHeight); 
+        txtState.hoveredCol = (int)std::floor(relX / txtState.charWidth); 
+
+        if (txtState.hoveredRow < 0) txtState.hoveredRow = 0; 
+        if (txtState.hoveredRow >= totalRows) txtState.hoveredRow = totalRows - 1; 
+        if (txtState.hoveredCol < 0) txtState.hoveredCol = 0; 
+
+        if (ImGui::IsMouseClicked(0)) {
+            int clicks = ImGui::GetMouseClickedCount(0); 
+
+            ctx.isActive = true; 
+            ctx.isCharMode = true; 
+            ctx.startRow = txtState.hoveredRow; 
+            ctx.endRow = txtState.hoveredRow; 
+
+            // select the entire line
+            if (clicks == 3) {
+                ctx.startCol = 0;
+                ctx.endCol = std::numeric_limits<int>::max(); 
+            }
+
+            // select a whole string 
+            else if (clicks == 2) {
+                txtState.isPendingWordSelect = true; 
+            }
+
+            else {
+                ctx.startCol = txtState.hoveredCol; 
+                ctx.endCol = txtState.hoveredCol; 
+            }
+        }
+
+        // handle the dragging of the mouse to select text 
+        else if (ImGui::IsMouseDown(0) && ctx.isActive && !ImGui::IsMouseClicked(0)) {
+            ctx.endCol = txtState.hoveredCol; 
+            ctx.endRow = txtState.hoveredRow; 
+        }
+
+    } 
+    return true; 
+}
+
+void TextSelector::Text(const std::string& text, int rowIndex, std::function<void()> customDraw) {
+    if (!txtState.userCtx) {
+        std::cerr << "Must Call 'TextSelector::Begin()' to draw the text box" << std::endl;  
+        return; 
     } 
 
-    // bottom line of multi-selection
-    else if (currRow == selMax) {
-        int eCol = (ctx.startRow < ctx.endRow) ? ctx.endCol : ctx.startCol; 
-        hlStart = 0.0f; 
-        hlEnd = (eCol + 1) * metrics.charWidth; 
+    // handle the double click logic and calculate the string that we need to select 
+    if (txtState.isPendingWordSelect && rowIndex == txtState.hoveredRow) {
+        int start, end; 
+        getWordUnderCursor(text, txtState.hoveredCol, start, end); 
+        txtState.userCtx->startCol = start; 
+        txtState.userCtx->endCol = end; 
+        txtState.isPendingWordSelect = false; 
     }
 
-    if (hlEnd > metrics.maxWidth) hlEnd = metrics.maxWidth; 
+    ImDrawList* drawList = ImGui::GetWindowDrawList(); 
+    float lineY = txtState.origin.y + (rowIndex * txtState.lineHeight); 
 
-    // draw highlight box around the text 
-    if (hlEnd > hlStart) {
-        float lineY = metrics.origin.y + (currRow * metrics.lineHeight);
+    // draw the selection highlight around the text 
+    if (txtState.userCtx->isActive) {
+        int relMin = std::min(txtState.userCtx->startRow, txtState.userCtx->endRow); 
+        int relMax = std::max(txtState.userCtx->startRow, txtState.userCtx->endRow); 
 
-        drawList->AddRectFilled (
-            ImVec2(metrics.origin.x + hlStart, lineY), 
-            ImVec2(metrics.origin.x + hlEnd, lineY + metrics.lineHeight), 
-            metrics.Color
-        ); 
+        if (rowIndex >= relMin && rowIndex <= relMax) {
+            float hlStart, hlEnd = 0.0f; 
+
+            // normalization of columns based on the direction of the rows 
+            int sCol = txtState.userCtx->startCol; 
+            int eCol = txtState.userCtx->endCol; 
+            if (txtState.userCtx->startRow > txtState.userCtx->endRow) std::swap(sCol, eCol); {
+                
+                // single line selection
+                if (relMin == relMax) {
+                    hlStart = std::min(sCol, eCol) * txtState.charWidth;
+                    hlEnd = (std::max(sCol, eCol) + 1) * txtState.charWidth;
+                }
+
+                // top row of multi-line selection
+                else if (rowIndex == relMin) {
+                    hlStart = sCol * txtState.charWidth; 
+                    hlEnd =txtState.maxWidth; 
+                }
+
+                // bottom row of mult-line selection
+                else if (rowIndex == relMax) {
+                    hlStart = 0.0f; 
+                    hlEnd = (eCol + 1) * txtState.charWidth; 
+                }
+
+                // middle row of multi-line 
+                else {
+                    hlStart = 0.0f; 
+                    hlEnd = txtState.maxWidth; 
+                }
+
+                float textW = (text.length() + 1) * txtState.charWidth; 
+                if (hlEnd > textW) hlEnd = textW; 
+
+                if (hlEnd > hlStart) {
+                    drawList->AddRectFilled(
+                        ImVec2(txtState.origin.x + hlStart, lineY), 
+                        ImVec2(txtState.origin.x + hlEnd, lineY + txtState.lineHeight), 
+                        txtState.highlightColor
+                    ); 
+                }
+            }
+        }
+    }
+
+    ImGui::SetCursorScreenPos(ImVec2(txtState.origin.x, lineY)); 
+
+    // here we execute the callback fn that allows use custom commands (e.g. have colored text segment)
+    if (customDraw) {
+        customDraw();
+    } else {
+        ImGui::TextUnformatted(text.c_str()); 
     }
 }
 
-void TextSelector::calcTextRowAndCol(const ImVec2& mousePos, const TextSelectionMetrics& metrics, int& outRow, int& outCol) {
-    float relX = mousePos.x - metrics.origin.x; 
-    float relY = mousePos.y - metrics.origin.y; 
-
-    outRow = (int)std::floor(relY / metrics.lineHeight); 
-    outCol = (int)std::floor(relX / metrics.charWidth); 
-
-    if (outRow < 0) outRow = 0; 
-    if (outCol < 0) outCol = 0; 
+void TextSelector::End() {
+    txtState.userCtx = nullptr; 
 }
 
+float TextSelector::GetLineHeight() {
+    return std::max(1.0f, ImGui::GetTextLineHeight());
+}
+
+// selection based on mouse clicks (e.g. two to select a string and three to select the entire line)
 void TextSelector::handleMouseClicks(TextSelectionCtx& ctx, int row, int col, const std::string& lineText) {
     int clickCount = ImGui::GetMouseClickedCount(0); 
 
-    ctx.active = true; 
+    ctx.isActive = true; 
     ctx.startRow = row; 
     ctx.endRow = row; 
 
@@ -127,15 +237,20 @@ void TextSelector::getWordUnderCursor(const std::string& text, int col, int& out
 }
 
 // helpers for mouse 2x clicking selection to decide how much of the substr to select 
-bool isWhiteSpace(char c) {
+bool TextSelector::isWhiteSpace(char c) {
     return c == ' ' || c == '\t' || c == '\n'; 
 }
-
-bool isDelimeter(char c) {
+bool TextSelector::isDelimeter(char c) {
     return c == '"' || c == '\'' || c == '(' || c == ')' ||
             c == '[' || c == ']'  || c == '{' || c == '}'; 
 }
-
 bool TextSelector::isToken(char c) {
     return !isWhiteSpace(c) && !isDelimeter(c); 
 } 
+
+// get the narrowest and widest characters and check the spacing between the two to see if the current font is monospace
+bool TextSelector::isFontMonospace() {
+    float w = ImGui::CalcTextSize("W").x;
+    float i = ImGui::CalcTextSize("i").x;
+    return std::abs(w - i) < 0.001f;  
+}
--- src/core/ui/components/TextSelector.hpp
+++ src/core/ui/components/TextSelector.hpp
@@ -3,40 +3,51 @@
 #include <algorithm>
 #include <cmath> 
 #include <string>
+#include <functional>
 
 // This only works if the text is monospaced however, the font that we use should already be monospaced.
 
 // stores information about dragging the cursor with the text boxes
 struct TextSelectionCtx {
-    bool active = false; 
+    bool isActive = false; 
+    bool isCharMode = false; 
+
     int startRow= -1; 
     int endRow = -1; 
+
     int startCol = 0; 
     int endCol = 0; 
 
+    void clear() {
+        isActive = false; 
+        isCharMode = false; 
+        startRow = -1; 
+        endRow = -1; 
+        startCol = 0; 
+        endCol = 0; 
+    }
+
     // helper to handle dragging the cursor upwards 
-    void getNormalizedRows(int& outMin, int& outMax) const {
-        outMin = std::min(startRow, endRow); 
-        outMax = std::max(startRow, endRow);
+    std::pair<int, int> getRange() const {
+        if (startRow <= endRow) 
+            return {startRow, endRow};
     }
 }; 
 
-struct TextSelectionMetrics {
-    float lineHeight = 0.0f; 
-    float charWidth = 0.0f; 
-    float maxWidth = 0.0f;                  
-    ImVec2 origin = ImVec2(0,0); 
-    ImU32 Color = IM_COL32(0, 120, 215, 100);   // default blue like in vscode 
-};
-
+// ImGui extension that I made to spawn ImGui::Text that we can select and copy the contents of 
 class TextSelector {
     public:
-    static void drawRowSelection(ImDrawList* drawList, const TextSelectionCtx& ctx, const TextSelectionMetrics& metrics, int currRow); 
-    static void calcTextRowAndCol(const ImVec2& mousePos, const TextSelectionMetrics& metrics, int& outRow, int& outCol);
-    static void handleMouseClicks(TextSelectionCtx& ctx, int row, int col, const std::string& lineText); 
-    static void getWordUnderCursor(const std::string& text, int col, int& outStart, int& outEnd); 
+    static bool Begin(const char* id, int totalRows, TextSelectionCtx& ctx, ImU32 highlightCol = IM_COL32(0, 120, 215, 100));
+    static void Text(const std::string& text, int rowIndex, std::function<void()> customDraw); 
+    static void End(); 
+    static float GetLineHeight(); 
+    static void copyText(); 
+    static std::string getSelectedText(); 
     private:
     static bool isWhiteSpace(char c); 
     static bool isDelimeter(char c); 
     static bool isToken(char c); 
+    static void handleMouseClicks(TextSelectionCtx& ctx, int row, int col, const std::string& lineText); 
+    static void getWordUnderCursor(const std::string& text, int col, int& outStart, int& outEnd); 
+    static bool isFontMonospace(); 
 };
