6ddee2980ffdf5b00215baea0f50331db45ef29f
argetlam <marrislucas@gmail.com>
2026-03-20 09:47:29 -0400

    Shutdown loop crashing

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,39 @@
 
 #define APPLICATION_TITLE "PrismTSS"
 
-int main(int argc, char** argv) {
-    AppContext ctx = AppContext(APPLICATION_TITLE);
+int main() {
+    bool keepRunning = true;
 
-    ctx.settings.userConfigDir = Paths::getUserConfigDir(APPLICATION_TITLE);
-    ctx.settings.settingsPath = ctx.settings.userConfigDir / "settings.json";
-    SettingsLoader::load(ctx.settings);
+    while (keepRunning) {
+        AppContext ctx = AppContext(APPLICATION_TITLE);
 
-    ctx.project.projectRoot = Paths::getProjectRootDir(ctx.settings.projectToOpen, ctx.project.projectTitle);
-    ctx.project.projectShadersDir = ctx.project.projectRoot / "shaders";
-    ctx.project.projectJSON = ctx.project.projectRoot / "project.json";
-    ctx.project.shaderRegistry = &ctx.shader_registry;
-    ctx.project.uniformRegistry = &ctx.uniform_registry;
-    ctx.project.events = &ctx.events;
+        ctx.settings.userConfigDir = Paths::getUserConfigDir(APPLICATION_TITLE);
+        ctx.settings.settingsPath = ctx.settings.userConfigDir / "settings.json";
+        SettingsLoader::load(ctx.settings);
 
-    if (!Application::initialize(ctx))
-    {
-        std::cerr << "Application failed to initialize" << std::endl;
-        return 1;
+        ctx.project.projectRoot = Paths::getProjectRootDir(ctx.settings.projectToOpen, ctx.project.projectTitle);
+        ctx.project.projectShadersDir = ctx.project.projectRoot / "shaders";
+        ctx.project.projectJSON = ctx.project.projectRoot / "project.json";
+        ctx.project.shaderRegistry = &ctx.shader_registry;
+        ctx.project.uniformRegistry = &ctx.uniform_registry;
+        ctx.project.events = &ctx.events;
+
+        if (!Application::initialize(ctx))
+        {
+            std::cerr << "Application failed to initialize" << std::endl;
+            return 1;
+        }
+        ProjectLoader::load(ctx.project);
+
+        Application::runLoop(ctx);
+        Application::shutdown(ctx);
+
+        if (!ctx.projectSwitch) ctx.settings.projectToOpen = ctx.project.projectTitle;
+        ProjectLoader::save(ctx.project);
+        SettingsLoader::save(ctx.settings);
+
+        if (!ctx.projectSwitch) keepRunning = false;
     }
-    ProjectLoader::load(ctx.project);
-
-    Application::runLoop(ctx);
-    Application::shutdown(ctx);
-
-    if (!ctx.projectSwitch) ctx.settings.projectToOpen = ctx.project.projectTitle;
-    ProjectLoader::save(ctx.project);
-    SettingsLoader::save(ctx.settings);
-
-    if (ctx.projectSwitch) main(argc, argv);
 
     return 0;
 }
